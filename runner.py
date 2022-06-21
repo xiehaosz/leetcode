7b7903446d16b98f28513bf937124ee4b3bd7720
xiehaosz <43995751+xiehaosz@users.noreply.github.com>
2022-06-21 12:17:21 +0800

    Create runner.py

---
+++ runner.py
@@ -0,0 +1,22 @@
+from leetcode_lv0 import *
+from leetcode_lv1 import *
+from leetcode_lv2 import *
+
+if __name__ == '__main__':
+
+    # Level-0
+    # No_0088_Merge.run()
+
+    # # Level-1
+    # No_0316_RemoveDuplicateLetters.run()
+    # No_0402_RemoveKDigits.run()
+    # No_0739_DailyTemperatures.run()
+    #
+    # # Level-2
+    No_0004_FindMedianSortedArrays.run()
+    # No_0032_LongestValidParentheses.run()
+    # No_0042_TrapRain.run()
+    # No_0084_LargestRectangleArea.run()
+    # No_0085_LargestRectangleArea.run()
+    # No_0321_MaxNumber.run()
+
